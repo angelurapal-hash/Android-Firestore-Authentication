aa9b85d2e27a458a7f322d4bfe8db1934ad89154
angelurapal-hash <angelu.rapal@hcdc.edu.ph>
2025-11-23 17:57:40 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false // puwede rin i-update sa 4.4.0+
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2") // latest stable
+    }
+}
